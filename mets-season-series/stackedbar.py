90de756d9280740c03a55bb6e979babbdfffd00a
Tanner Murphey <ctmurphey@protonmail.com>
2022-07-04 05:14:24 -0400

    added total season progress tracker

--- mets-season-series/stackedbar.py
+++ mets-season-series/stackedbar.py
@@ -82,60 +82,93 @@ incomplete_away = round(inc_away)
 
 
 
-fig, ax = plt.subplots(figsize=(25, 25))
+# fig, ax= plt.subplots(figsize=(25, 25), nrows=2, ncols=1, gridspec_kw={'height_ratios': [5, 1]})
+fig, ax= plt.subplots(nrows=2, ncols=1, gridspec_kw={'height_ratios': [15, 1]})
 
 w = 0.9 #main bar width
 x = np.arange(len(team_set))
 
 
-wins       = ax.barh(x, df_teams['won'], w, label='won',
+wins       = ax[0].barh(x, df_teams['won'], w, label='won',
                      color="cornflowerblue", edgecolor='k')
-losses     = ax.barh(x, df_teams['lost'], w, left=df_teams['won'],
+losses     = ax[0].barh(x, df_teams['lost'], w, left=df_teams['won'],
                      label='lost', color="darkorange", edgecolor='k')
 # incomp = ax.barh(x-w/4, df_teams['incomplete'], w/2, 
 #                     left=df_teams['won']+df_teams['lost'], label="incomplete total", color='silver')
 
-home       = ax.barh(x, df_teams['inc_home'], w, left=df_teams['won']+df_teams['lost'],
+home       = ax[0].barh(x, df_teams['inc_home'], w, left=df_teams['won']+df_teams['lost'],
                      label="incomplete home", color='silver', edgecolor='k')
-away       = ax.barh(x, df_teams['inc_away'], w, left=df_teams['won']+df_teams['lost'] + df_teams['inc_home'],
+away       = ax[0].barh(x, df_teams['inc_away'], w, left=df_teams['won']+df_teams['lost'] + df_teams['inc_home'],
                      label="incomplete away", color='k', edgecolor='k')
 
 for index, row in df_teams.iterrows():
     # So we ignore when one of the totals equals zero
     if round(row['won']) != 0:
-        ax.text(row['won']/2, x[index], int(row['won']),
+        ax[0].text(row['won']/2, x[index], int(row['won']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
     
     if round(row['lost']) != 0:
-        ax.text(row['won']+row['lost']/2, x[index], int(row['lost']),
+        ax[0].text(row['won']+row['lost']/2, x[index], int(row['lost']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
     if round(row['inc_home']) != 0:
-        ax.text(row['won']+row['lost']+row['inc_home']/2, x[index], int(row['inc_home']),
+        ax[0].text(row['won']+row['lost']+row['inc_home']/2, x[index], int(row['inc_home']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
     if round(row['inc_away']) != 0:
-        ax.text(row['won']+row['lost']+row['inc_home']+row['inc_away']/2, x[index], int(row['inc_away']),
+        ax[0].text(row['won']+row['lost']+row['inc_home']+row['inc_away']/2, x[index], int(row['inc_away']),
                 weight='bold', va='center', ha='center', color='w',
                 size=18, fontname='serif')
     
-   
+
+
+###Edits to add total season bar are here
+
+wins       = ax[1].barh(0, won, w, label='won',
+                     color="cornflowerblue", edgecolor='k')
+ax[1].text(won/2, 0, round(won) ,weight='bold', va='center', ha='center',
+                size=18, fontname='serif')
+
+losses     = ax[1].barh(0, lost, w, left=won,
+                     label='lost', color="darkorange", edgecolor='k')
+ax[1].text(won+lost/2, 0, round(lost), weight='bold', va='center', ha='center',
+                size=18, fontname='serif')
+
+home       = ax[1].barh(0, inc_home, w, left=won+lost,
+                     label="incomplete home", color='silver', edgecolor='k')
+ax[1].text(won+lost+inc_home/2, 0, round(inc_home),
+                weight='bold', va='center', ha='center',
+                size=18, fontname='serif')
+
+away       = ax[1].barh(0, inc_away, w, left=won+lost+inc_home,
+                     label="incomplete away", color='k', edgecolor='k')
+ax[1].text(won+lost+inc_home+inc_away/2, 0, round(inc_away),
+                weight='bold', va='center', ha='center', color='w',
+                size=18, fontname='serif')
+
+ax[1].axis('off')
+ax[1].set_title('Total Season:', size=20, weight='bold',
+             fontname='serif')
+
+ax[1].set_xlim(0, 162)
+###End edits
+
 #Graph styling:
 
 
 
-ax.grid(axis='x', which='major', linewidth=2)
-ax.set_ylim(min(x)-w/2, max(x)+w/2)
-ax.invert_yaxis()
-ax.set_xticks(range(0, 21, 5), fontfamily='serif', minor=False)
-ax.set_xticks(range(0, 21, 1), fontfamily='serif', minor=True)
-ax.tick_params('x', labelsize='large')
-ax.set_yticks(ticks = x, labels=df_teams['team'], size=18, fontfamily='serif')
-ax.set_xlim(0, 20)
-ax.legend(labels=['won', 'lost', 'incomplete home', 'incomplete away'],
+ax[0].grid(axis='x', which='major', linewidth=2)
+ax[0].set_ylim(min(x)-w/2, max(x)+w/2)
+ax[0].invert_yaxis()
+ax[0].set_xticks(range(0, 21, 5), fontfamily='serif', minor=False)
+ax[0].set_xticks(range(0, 21, 1), fontfamily='serif', minor=True)
+ax[0].tick_params('x', labelsize='large')
+ax[0].set_yticks(ticks = x, labels=df_teams['team'], size=18, fontfamily='serif')
+ax[0].set_xlim(0, 20)
+ax[0].legend(labels=['won', 'lost', 'incomplete home', 'incomplete away'],
           loc=4, ncol=1, prop={'family': "serif", 'size':'x-large', 'weight': 'bold'})
 
 fig.suptitle(f"Mets Current Season Series Progress\n{date.today()} ({won}-{lost}, {incomplete} GR)", size=24, weight='bold',
