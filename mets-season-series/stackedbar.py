e56c70c24c1bdd22f87483b2835c6623a738f75e
Chauncey Tanner Murphey <47878298+tanner-murphey@users.noreply.github.com>
2023-03-24 03:15:30 -0400

    looks terrible but works

--- mets-season-series/stackedbar.py
+++ mets-season-series/stackedbar.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pybaseball as pbb
 from datetime import date
+from matplotlib import gridspec
 
 
 
@@ -58,11 +59,12 @@ for index, row in df_sch.iterrows():
     else:
         df_teams.loc[df_teams['team']==row['other_team'], ['lost']] += 1    
 
-    df_teams.loc[df_teams['team']==row['other_team'], ['total']] += 1   
+    df_teams.loc[df_teams['team']==row['other_team'], ['total']] += 1  
 
 
-standings = pd.concat(pbb.standings(), ignore_index=True)
+### Preseason messes up pybaseball.standings:
 try:
+    standings = pd.concat(pbb.standings(), ignore_index=True)
     team_rec = [f"{standings.loc[standings['Tm']==team, ['W']].values[0][0]}-{standings.loc[standings['Tm']==team, ['L']].values[0][0]}" for team in df_teams['team']]
 except:
     team_rec = ["0-0" for team in df_teams['team']]
@@ -70,7 +72,7 @@ except:
 
 nicknames = []
 for i,t in enumerate(df_teams['team']):
-    if t.split()[-1] == "Sox": #Accounting for Boston/Chicago
+    if t.split()[-1] == "Sox": #Accounting for Boston/Chicago Sox
         nicknames.append(f"{t.split()[-2]+ ' ' + t.split()[-1] } ({team_rec[i]})")
     else:
         nicknames.append(f"{t.split()[-1]} ({team_rec[i]})")
@@ -90,99 +92,110 @@ incomplete_away = round(inc_away)
 
 
 
+###
 
 
 # fig, ax= plt.subplots(figsize=(25, 25), nrows=2, ncols=1, gridspec_kw={'height_ratios': [5, 1]})
-fig, ax= plt.subplots(nrows=2, ncols=1, gridspec_kw={'height_ratios': [30, 1]})
-
+fig, ax= plt.subplots(nrows=2, ncols=2, gridspec_kw={'height_ratios': [15, 1]})
+ax[1,1].remove()
 w = 0.9 #main bar width
-x = np.arange(len(team_set))
+x = [np.arange(15), np.arange(14)]
+
+for i in range(2):
+    wins       = ax[0,i].barh(x[i], df_teams['won'][i::2], w, label='won',
+                        color="cornflowerblue", edgecolor='k')
+    losses     = ax[0,i].barh(x[i], df_teams['lost'][i::2], w, left=df_teams['won'][i::2],
+                        label='lost', color="darkorange", edgecolor='k')
 
 
-wins       = ax[0].barh(x, df_teams['won'], w, label='won',
-                     color="cornflowerblue", edgecolor='k')
-losses     = ax[0].barh(x, df_teams['lost'], w, left=df_teams['won'],
-                     label='lost', color="darkorange", edgecolor='k')
-
-
-home       = ax[0].barh(x, df_teams['inc_home'], w, left=df_teams['won']+df_teams['lost'],
-                     label="incomplete home", color='silver', edgecolor='k')
-away       = ax[0].barh(x, df_teams['inc_away'], w, left=df_teams['won']+df_teams['lost'] + df_teams['inc_home'],
-                     label="incomplete away", color='k', edgecolor='k')
+    home       = ax[0,i].barh(x[i], df_teams['inc_home'][i::2], w, left=df_teams['won'][i::2]+df_teams['lost'][i::2],
+                        label="incomplete home", color='silver', edgecolor='k')
+    away       = ax[0,i].barh(x[i], df_teams['inc_away'][i::2], w, left=df_teams['won'][i::2]+df_teams['lost'][i::2] + df_teams['inc_home'][i::2],
+                        label="incomplete away", color='k', edgecolor='k')
 
 for index, row in df_teams.iterrows():
+    # print(row)
     # So we ignore when one of the totals equals zero
     if round(row['won']) != 0:
-        ax[0].text(row['won']/2, x[index], int(row['won']),
+        ax[0,index%2].text(row['won']/2, x[0,index//2], int(row['won']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
     
     if round(row['lost']) != 0:
-        ax[0].text(row['won']+row['lost']/2, x[index], int(row['lost']),
+        ax[0,index%2].text(row['won']+row['lost']/2, x[0,index//2], int(row['lost']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
     if round(row['inc_home']) != 0:
-        ax[0].text(row['won']+row['lost']+row['inc_home']/2, x[index], int(row['inc_home']),
+        ax[0,index%2].text(row['won']+row['lost']+row['inc_home']/2,
+                x[0][index//2], int(row['inc_home']),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
     if round(row['inc_away']) != 0:
-        ax[0].text(row['won']+row['lost']+row['inc_home']+row['inc_away']/2, x[index], int(row['inc_away']),
+        ax[0,index%2].text(row['won']+row['lost']+row['inc_home']+row['inc_away']/2,
+                x[0][index//2], int(row['inc_away']),
                 weight='bold', va='center', ha='center', color='w',
                 size=18, fontname='serif')
     
 
 
-###Edits to add total season bar are here
+###total season bar
 
-wins       = ax[1].barh(0, won, w, label='won',
+wins       = ax[1,0].barh(0, won, w, label='won',
                      color="cornflowerblue", edgecolor='k')
-ax[1].text(won/2, 0, round(won) ,weight='bold', va='center', ha='center',
+ax[1,0].text(won/2, 0, round(won) ,weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
-losses     = ax[1].barh(0, lost, w, left=won,
+losses     = ax[1,0].barh(0, lost, w, left=won,
                      label='lost', color="darkorange", edgecolor='k')
-ax[1].text(won+lost/2, 0, round(lost), weight='bold', va='center', ha='center',
+ax[1,0].text(won+lost/2, 0, round(lost), weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
-home       = ax[1].barh(0, inc_home, w, left=won+lost,
+home       = ax[1,0].barh(0, inc_home, w, left=won+lost,
                      label="incomplete home", color='silver', edgecolor='k')
-ax[1].text(won+lost+inc_home/2, 0, round(inc_home),
+ax[1,0].text(won+lost+inc_home/2, 0, round(inc_home),
                 weight='bold', va='center', ha='center',
                 size=18, fontname='serif')
 
-away       = ax[1].barh(0, inc_away, w, left=won+lost+inc_home,
+away       = ax[1,0].barh(0, inc_away, w, left=won+lost+inc_home,
                      label="incomplete away", color='k', edgecolor='k')
-ax[1].text(won+lost+inc_home+inc_away/2, 0, round(inc_away),
+ax[1,0].text(won+lost+inc_home+inc_away/2, 0, round(inc_away),
                 weight='bold', va='center', ha='center', color='w',
                 size=18, fontname='serif')
 
-ax[1].axis('off')
-ax[1].set_title('Total Season:', size=20, weight='bold',
+ax[1,0].axis('off')
+ax[1,0].set_title('Total Season:', size=20, weight='bold',
              fontname='serif')
 
-ax[1].set_xlim(0, 162)
+ax[1,0].set_xlim(0, 162)
 ###End edits
 
 #Graph styling:
 
 
 
-ax[0].grid(axis='x', which='major', linewidth=2)
-ax[0].set_ylim(min(x)-w/2, max(x)+w/2)
-ax[0].invert_yaxis()
-ax[0].set_xticks(range(0, 21, 5), fontfamily='serif', minor=False)
-ax[0].set_xticks(range(0, 21, 1), fontfamily='serif', minor=True)
-ax[0].tick_params('x', labelsize='large')
-ax[0].set_yticks(ticks = x, labels=df_teams['team'], size=18, fontfamily='serif')
-ax[0].set_xlim(0, df_teams['total'].max())
-ax[0].legend(labels=['won', 'lost', 'incomplete home', 'incomplete away'],
+# ax[0,0].grid(axis='x', which='major', linewidth=2)
+for i in range(2):
+    ax[0,i].set_ylim(min(x[0])-w/2, max(x[0])+w/2)
+    ax[0,i].invert_yaxis()
+    ax[0,i].set_xticks(range(0, 21, 5), fontfamily='serif', minor=False)
+    ax[0,i].set_xticks(range(0, 21, 1), fontfamily='serif', minor=True)
+    ax[0,i].tick_params('x', labelsize='large')
+    ax[0,i].set_yticks(ticks = x[i], labels=df_teams['team'][i::2], size=18, fontfamily='serif')
+    ax[0,i].set_xlim(0, df_teams['total'].max())
+ax[0,0].legend(labels=['won', 'lost', 'incomplete home', 'incomplete away'],
           loc=4, ncol=1, prop={'family': "serif", 'size':'x-large', 'weight': 'bold'})
 
 fig.suptitle(f"Mets Current Season Series Progress\n{date.today()} ({won}-{lost}, {incomplete} GR)", size=24, weight='bold',
              fontname='serif', y=0.9, va='bottom')
+plt.subplots_adjust(wspace=0.5, right=0.995, left=0.21)
 
-fig.tight_layout()
-plt.savefig("test.png")
+
+### Legend plot
+
+
+
+
+# plt.savefig("test.png")
 plt.show()
